55e8e40f770f28b43d36cb09b7986848d28a6a36
ChiaNingv <dozeuto@gmail.com>
2021-07-11 21:45:17 +0800

    Add FFFloatingTextFieldView file.

---
+++ FFFloatingTextFieldView/FFFloatingTextFieldView.swift
@@ -0,0 +1,22 @@
+//
+//  FFFloatingTextFieldView.swift
+//  FFFloatingTextFieldView
+//
+//  Created by 贾发 on 2021/7/11.
+//
+
+import UIKit
+
+class FFFloatingTextFieldView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+
+    
+}
